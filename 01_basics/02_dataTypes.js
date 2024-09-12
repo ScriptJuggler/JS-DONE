66ad0dc5bd2a2177ce20c9674cc1ec883e4844ac
Tamal Saha <sahatamal039@gmail.com>
2024-09-12 03:15:18 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS Code as newer version
+
+// number => 2 to power 53
+//bigint
+//string => 1111
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+// object
+
+console.log(typeof 23)
+console.log(typeof null)
+console.log(typeof undefined)
