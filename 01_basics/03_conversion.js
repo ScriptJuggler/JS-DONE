b5cd33bddb70891063486973c5e147c705e3424b
Tamal Saha <sahatamal039@gmail.com>
2024-09-12 04:04:37 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,24 @@
+let score = "33ab";
+
+let scoreNum = Number(score);
+console.log(typeof(scoreNum));
+console.log(scoreNum);
+
+//Number Conversion
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// Boolean conversion
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
